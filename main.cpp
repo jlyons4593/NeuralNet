f972e7b48300cbbc41280ced44083c0fbf4ac5d9
jlyons4593 <jlyons4593@gmail.com>
2023-01-01 18:54:04 +0000

    Created Neural Network object

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<NeuralNetwork.hpp>
+
+void initializeModelArchitecture(){
+   
+}
+
+int main() {
+   int num_inputs = 3;
+    int num_outputs = 1;
+    std::vector<int> layer_sizes = {4, 5, 6};
+
+    NeuralNetwork nn(num_inputs, num_outputs, layer_sizes);
+
+}
