f0593401a84c20db66751e863a1dd2b9bbd51e46
jlyons4593 <jlyons4593@gmail.com>
2023-01-01 19:32:17 +0000

    fixing files

--- main.cpp
+++ main.cpp
@@ -1,15 +1,17 @@
 #include<iostream>
-#include<NeuralNetwork.hpp>
+#include "NeuralNetwork.hpp"
+#include <vector>
 
 void initializeModelArchitecture(){
    
 }
 
 int main() {
-   int num_inputs = 3;
+    int num_inputs = 3;
     int num_outputs = 1;
     std::vector<int> layer_sizes = {4, 5, 6};
 
     NeuralNetwork nn(num_inputs, num_outputs, layer_sizes);
-
+    std::cout << "working..." << std::endl;
 }
+
