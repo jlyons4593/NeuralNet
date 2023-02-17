f1f68b8c6a49c0dcc389b6e7310daea2768e6b4c
jlyons4593 <jlyons4593@gmail.com>
2023-02-17 08:28:48 +0000

    basic neural net completed

--- main.cpp
+++ main.cpp
@@ -1,46 +1,252 @@
-#include<iostream>
-#include "NeuralNetwork.hpp"
 #include <vector>
-
-#include <string>
+#include <iostream>
+#include <cstdlib>
 #include <cmath>
+#include <fstream>
+#include <sstream>
 
-<<<<<<< HEAD
-=======
+using namespace std;
 
-void initializeModelArchitecture(){
-   
+struct Edge
+{
+    double weight;
+    double deltaWeight;
+};
+
+
+class Neuron;
+
+typedef std::vector<Neuron> Layer;
+
+class Neuron
+{
+public:
+    Neuron(unsigned numOutputs, unsigned myIndex);
+    void setOutputVal(double val) { m_outputVal = val; }
+    double getOutputVal(void) const { return m_outputVal; }
+    void feedForward(const Layer &prevLayer);
+    void calcOutputGradients(double targetVal);
+    void calcHiddenGradients(const Layer &nextLayer);
+    void updateInputWeights(Layer &prevLayer);
+
+private:
+    static double LearningRate;  
+    static double alpha; 
+    static double transferFunction(double x);
+    static double transferFunctionDerivative(double x);
+    static double randomWeight(void) { return rand() / double(RAND_MAX); }
+    double sumDerivativesOfWeights(const Layer &nextLayer) const;
+    double m_outputVal;
+    std::vector<Edge> m_outputWeights;
+    unsigned m_myIndex;
+    double m_gradient;
+};
+
+double Neuron::LearningRate = 0.15;  
+double Neuron::alpha = 0.5;   
+
+
+void Neuron::updateInputWeights(Layer &prevLayer)
+{
+
+    for (unsigned n = 0; n < prevLayer.size(); ++n) {
+        Neuron &neuron = prevLayer[n];
+        double oldDeltaWeight = neuron.m_outputWeights[m_myIndex].deltaWeight;
+
+        double newDeltaWeight =
+
+        neuron.m_outputWeights[m_myIndex].deltaWeight = newDeltaWeight;
+        neuron.m_outputWeights[m_myIndex].weight += newDeltaWeight;
+    }
 }
->>>>>>> bd813b80d4f061c6566473ae71d2fb9f678d8fd5
+
+double Neuron::sumDerivativesOfWeights(const Layer &nextLayer) const
+{
+    double sum = 0.0;
 
 
+    for (unsigned n = 0; n < nextLayer.size() - 1; ++n) {
+        sum += m_outputWeights[n].weight * nextLayer[n].m_gradient;
+    }
 
-int main() {
+    return sum;
+}
 
-    int num_inputs = 3;
-    int num_outputs = 1;
-    
-    std::vector<int> layer_sizes = {64, 64, 64, 64};
+void Neuron::calcHiddenGradients(const Layer &nextLayer)
+{
+    double derivativesOfWeights = sumDerivativesOfWeights(nextLayer);
+    m_gradient = derivativesOfWeights * Neuron::transferFunctionDerivative(m_outputVal);
+}
 
-    NeuralNetwork nn(num_inputs, num_outputs, layer_sizes);
-<<<<<<< HEAD
-    
-    std::vector<std::vector<float>> biases = nn.getBiases();
-    
+void Neuron::calcOutputGradients(double targetVal)
+{
+    double delta = targetVal - m_outputVal;
+    m_gradient = delta * Neuron::transferFunctionDerivative(m_outputVal);
+}
 
-    std::cout << std::to_string(biases[2][2])<< std::endl;
-=======
+double Neuron::transferFunction(double x)
+{
 
-    nn.initialize();
-    std::vector<std::vector<float>> biases  = nn.getBiases();
-    for (std::vector i: biases){
-        for (float j: i){
-            
-            std::cout << std::to_string(j) << std::endl;
+    return tanh(x);
+}
+
+double Neuron::transferFunctionDerivative(double x)
+{
+    return 1.0 - x * x;
+}
+
+void Neuron::feedForward(const Layer &prevLayer)
+{
+    double sum = 0.0;
+
+    for (unsigned n = 0; n < prevLayer.size(); ++n) {
+        sum += prevLayer[n].getOutputVal() *
+                prevLayer[n].m_outputWeights[m_myIndex].weight;
+    }
+
+    m_outputVal = Neuron::transferFunction(sum);
+}
+
+Neuron::Neuron(unsigned numOutputs, unsigned myIndex)
+{
+    for (unsigned c = 0; c < numOutputs; ++c) {
+        m_outputWeights.push_back(Edge());
+        m_outputWeights.back().weight = randomWeight();
+    }
+
+    m_myIndex = myIndex;
+}
+
+
+class NeuralNetwork
+{
+public:
+    NeuralNetwork(const std::vector<unsigned> &topology);
+    void feedForward(const std::vector<double> &inputVals);
+    void backProp(const std::vector<double> &targetVals);
+    void getResults(std::vector<double> &resultVals) const;
+    double getRecentAverageError(void) const { return m_averageError; }
+
+private:
+    std::vector<Layer> m_layers; // m_layers[layerNum][neuronNum]
+    double m_error;
+    double m_averageError;
+    static double m_smoothingFactor;
+};
+
+
+double NeuralNetwork::m_smoothingFactor = 100.0;
+
+
+void NeuralNetwork::getResults(std::vector<double> &resultVals) const
+{
+    resultVals.clear();
+
+    for (unsigned n = 0; n < m_layers.back().size() - 1; ++n) {
+        resultVals.push_back(m_layers.back()[n].getOutputVal());
+    }
+}
+
+void NeuralNetwork::backProp(const std::vector<double> &targetVals)
+{
+
+    Layer &outputLayer = m_layers.back();
+    m_error = 0.0;
+
+    for (unsigned n = 0; n < outputLayer.size() - 1; ++n) {
+        double delta = targetVals[n] - outputLayer[n].getOutputVal();
+        m_error += delta * delta;
+    }
+    m_error /= outputLayer.size() - 1; // get average error squared
+    m_error = sqrt(m_error); // RMS
+
+    m_averageError =
+            (m_averageError * m_smoothingFactor + m_error)
+            / (m_smoothingFactor + 1.0);
+
+    for (unsigned n = 0; n < outputLayer.size() - 1; ++n) {
+        outputLayer[n].calcOutputGradients(targetVals[n]);
+    }
+
+    for (unsigned layerNum = m_layers.size() - 2; layerNum > 0; --layerNum) {
+        Layer &hiddenLayer = m_layers[layerNum];
+        Layer &nextLayer = m_layers[layerNum + 1];
+
+        for (unsigned n = 0; n < hiddenLayer.size(); ++n) {
+            hiddenLayer[n].calcHiddenGradients(nextLayer);
         }
     }
->>>>>>> bd813b80d4f061c6566473ae71d2fb9f678d8fd5
-    
 
+    for (unsigned layerNum = m_layers.size() - 1; layerNum > 0; --layerNum) {
+        Layer &layer = m_layers[layerNum];
+        Layer &prevLayer = m_layers[layerNum - 1];
+
+        for (unsigned n = 0; n < layer.size() - 1; ++n) {
+            layer[n].updateInputWeights(prevLayer);
+        }
+    }
 }
 
+void NeuralNetwork::feedForward(const std::vector<double> &inputVals)
+{
+    
+
+    for (unsigned i = 0; i < inputVals.size(); ++i) {
+        m_layers[0][i].setOutputVal(inputVals[i]);
+    }
+
+    for (unsigned layerNum = 1; layerNum < m_layers.size(); ++layerNum) {
+        Layer &prevLayer = m_layers[layerNum - 1];
+        for (unsigned n = 0; n < m_layers[layerNum].size() - 1; ++n) {
+            m_layers[layerNum][n].feedForward(prevLayer);
+        }
+    }
+}
+
+NeuralNetwork::NeuralNetwork(const std::vector<unsigned> &topology)
+{
+    unsigned numLayers = topology.size();
+    for (unsigned layerNum = 0; layerNum < numLayers; ++layerNum) {
+        m_layers.push_back(Layer());
+        unsigned numOutputs = layerNum == topology.size() - 1 ? 0 : topology[layerNum + 1];
+
+        for (unsigned neuronNum = 0; neuronNum <= topology[layerNum]; ++neuronNum) {
+            m_layers.back().push_back(Neuron(numOutputs, neuronNum));
+            cout << "Made a Neuron!" << endl;
+        }
+
+        m_layers.back().back().setOutputVal(1.0);
+    }
+}
+
+
+void showVectorVals(string label, std::vector<double> &v)
+{
+    cout << label << " ";
+    for (unsigned i = 0; i < v.size(); ++i) {
+        cout << v[i] << " ";
+    }
+
+    cout << endl;
+}
+int main() {
+    
+ 
+   
+    //std::vector of ints for number of neurons per layer
+    std::vector<unsigned> topology;
+    topology.push_back(3);
+    topology.push_back(2);
+    topology.push_back(1);
+    NeuralNetwork nn(topology);
+    
+    std::vector<double> inputs;
+    nn.feedForward(inputs);
+    std::vector<double> targetVals;
+    nn.backProp(targetVals);
+    std::vector<double> resultVals;
+    nn.getResults(resultVals);
+    
+
+
+}
